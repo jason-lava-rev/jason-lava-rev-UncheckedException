abbde975d2acd99bb408422fe23ae3241067a271
jason-lava-rev <lavandowskij@gmail.com>
2023-03-11 14:54:57 +0000

    add arry out of bounds println

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -14,6 +14,7 @@ public class Lab {
      * event happens, like running out of computer memory.
      */
     public void throwUncheckedException(){
-
+        int x[] = {0,1,2,3,4,5};
+        System.out.println(x[99]);
     }
 }
